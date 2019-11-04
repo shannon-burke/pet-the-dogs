c06cdb6ed8cd8d7d9f540f8765b46f3fb2851a41
Shannon <shannonburke.art@gmail.com>
2019-11-03 20:49:55 -0600

    Updates

---
+++ precache-manifest.b8b0c1f448bb3c1f06329d0e769ad424.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac737e5750d2ec9434cacea3e45125cc",
+    "url": "/pet-the-dogs/index.html"
+  },
+  {
+    "revision": "6eb7f381944e243c953b",
+    "url": "/pet-the-dogs/static/css/main.2331858c.chunk.css"
+  },
+  {
+    "revision": "87a6ecb5869f102d0372",
+    "url": "/pet-the-dogs/static/js/2.0c1375de.chunk.js"
+  },
+  {
+    "revision": "6eb7f381944e243c953b",
+    "url": "/pet-the-dogs/static/js/main.587de086.chunk.js"
+  },
+  {
+    "revision": "565430f0e8c16af85c0a",
+    "url": "/pet-the-dogs/static/js/runtime-main.a89bf5e1.js"
+  }
+]);
